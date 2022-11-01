1fd3c2638af7f5cc6680fc8c89b7a9d63ab2744b
sky <jdisjajfid@gmail.com>
2022-11-01 11:31:33 +0800

    注释主类

--- FtpUtli/src/main/java/com/lc/ftp/FtpApplication.java
+++ FtpUtli/src/main/java/com/lc/ftp/FtpApplication.java
@@ -1,53 +1,53 @@
-package com.lc.ftp;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.annotation.Resource;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-
-@SpringBootApplication
-public class FtpApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(FtpApplication.class, args);
-    }
-
-    @Resource
-    private FtpTemplate ftpTemplate;
-
-    /**
-     * 上传图片
-     *
-     * @return
-     */
-//    @GetMapping("upload")
-    public Boolean upload() {
-        InputStream inputStream = null;
-        try {
-            inputStream = new FileInputStream("C:\\Users\\win10\\Pictures\\test.jpg");
-            return ftpTemplate.upload(inputStream, "/t", "tests.jpg");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
-    /**
-     * 删除图片
-     *
-     * @return
-     */
-//    @GetMapping("delete")
-    public Boolean delete() {
-        try {
-            return ftpTemplate.delete("/t", "tests.jpg");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-}
+//package com.lc.ftp;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//import javax.annotation.Resource;
+//import java.io.FileInputStream;
+//import java.io.FileNotFoundException;
+//import java.io.InputStream;
+//
+//@SpringBootApplication
+//public class FtpApplication {
+//    public static void main(String[] args) {
+//        SpringApplication.run(FtpApplication.class, args);
+//    }
+//
+//    @Resource
+//    private FtpTemplate ftpTemplate;
+//
+//    /**
+//     * 上传图片
+//     *
+//     * @return
+//     */
+////    @GetMapping("upload")
+//    public Boolean upload() {
+//        InputStream inputStream = null;
+//        try {
+//            inputStream = new FileInputStream("C:\\Users\\win10\\Pictures\\test.jpg");
+//            return ftpTemplate.upload(inputStream, "/t", "tests.jpg");
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return false;
+//    }
+//
+//    /**
+//     * 删除图片
+//     *
+//     * @return
+//     */
+////    @GetMapping("delete")
+//    public Boolean delete() {
+//        try {
+//            return ftpTemplate.delete("/t", "tests.jpg");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return false;
+//    }
+//}
